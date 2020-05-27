2e9fd9e50ace5f9b2c305fc14bc541f4e016e793
duxinyu <418714451@qq.com>
2020-05-27 20:38:40 +0800

    无用import

--- src/main/java/com/bigScreen/business/service/SecondPageServiceImp.java
+++ src/main/java/com/bigScreen/business/service/SecondPageServiceImp.java
@@ -1,37 +1,21 @@
 package com.bigScreen.business.service;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
+import com.bigScreen.business.dao.InterFirstViewMapper;
+import com.bigScreen.business.dao.SecondPageMapper;
+import com.bigScreen.business.dao.ThirdPageMapper;
+import com.bigScreen.business.model.SecondModel;
+import com.bigScreen.business.util.DateUtill;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import java.text.DecimalFormat;
-import java.text.NumberFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
-
-import com.bigScreen.business.dao.InterFirstViewMapper;
-import com.bigScreen.business.dao.SecondPageMapper;
-import com.bigScreen.business.model.SecondModel;
-import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
-
-import com.bigScreen.business.dao.ClassMapper;
-import com.bigScreen.business.dao.ThirdPageMapper;
-import com.bigScreen.business.entity.ClassEntity;
-import com.bigScreen.business.entity.WeatherEntity;
-import com.bigScreen.business.model.ThirdModel;
-import com.bigScreen.business.util.DateUtill;
-import com.bigScreen.business.util.HttpClient;
-import com.bigScreen.business.util.JSONUtil;
-import com.bigScreen.business.util.Utility;
-
-import net.sf.json.JSONArray;
-import net.sf.json.JSONObject;
 
 @Service
 public class SecondPageServiceImp implements SecondPageService{
